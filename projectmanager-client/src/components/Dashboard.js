e1ee3b25ac271f7b590b25b04bcadd20cff09777
jose <jose.l.morales@stonybrook.edu>
2019-09-16 16:34:06 -0400

    using data from get all projects set state and props in dashboard and generate ProjectItem with appropriate data from database

--- projectmanager-client/src/components/Dashboard.js
+++ projectmanager-client/src/components/Dashboard.js
@@ -11,6 +11,7 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { projects } = this.props.project;
     return (
       <div className="projects">
         <div className="container">
@@ -21,7 +22,9 @@ class Dashboard extends Component {
               <CreateProjectButton />
               <br />
               <hr />
-              <ProjectItem />
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
