0e010f7cdf287299c4b6835688cfe50693181b66
jose <jose.l.morales@stonybrook.edu>
2019-09-16 23:12:08 -0400

    setup deleting project and updating state using redux action and reducer

--- projectmanager-client/src/actions/projectActions.js
+++ projectmanager-client/src/actions/projectActions.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
     //do post if it works redirect to dashboard and dispatch
-    const res = await axios.post("http://localhost:8080/api/project", project);
+    const res = await axios.post("/api/project", project);
     history.push("/dashboard");
     //wipe state of errors if updated
     dispatch({ type: GET_ERRORS, payload: {} });
@@ -14,15 +14,31 @@ export const createProject = (project, history) => async dispatch => {
 };
 
 export const getProjects = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080/api/project/all");
+  const res = await axios.get("/api/project/all");
   dispatch({ type: GET_PROJECTS, payload: res.data });
 };
 
 export const getProject = (id, history) => async dispatch => {
   try {
-    const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+    const res = await axios.get(`/api/project/${id}`);
     dispatch({ type: GET_PROJECT, payload: res.data });
   } catch (error) {
     history.push("/dashboard");
+  }
+};
+
+export const deleteProject = id => async dispatch => {
+  if (
+    window.confirm(
+      "Warning, This will delete all data associate with this project"
+    )
+  ) {
+    //delete in server
+    await axios.delete(`/api/project/${id}`);
+    //return dispatch to reduce and correct state
+    dispatch({
+      type: DELETE_PROJECT,
+      payload: id
+    });
   }
 };
--- projectmanager-client/src/reducers/projectReducer.js
+++ projectmanager-client/src/reducers/projectReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROJECTS, GET_PROJECT } from "../actions/types";
+import { GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "../actions/types";
 
 const initialState = {
   projects: [],
@@ -16,6 +16,14 @@ export default function(state = initialState, action) {
       return {
         ...state,
         project: action.payload
+      };
+    //return state and filter out from list of projects using id of deleted project
+    case DELETE_PROJECT:
+      return {
+        ...state,
+        projects: state.projects.filter(
+          project => project.projectIdentifier !== action.payload
+        )
       };
     default:
       return state;
